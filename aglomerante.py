a3e984aa213ee5042c424a489d3d3f120683ad07
Aurelio Marín <aurelio.marin@iest.edu.mx>
2023-10-17 11:21:21 -0600

    initial commit

---
+++ aglomerante.py
@@ -0,0 +1,40 @@
+# https://www.jacobsoft.com.mx/es_mx/clustering-jerarquico-con-python/
+
+# Clustering Jerárquico
+
+# Importacion de librerias
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Carga del conjunto de datos
+dataset = pd.read_csv('Clientes_Tienda.csv')
+X = dataset.iloc[:, [3, 4]].values
+
+# Creamos el dendograma para encontrar el número óptimo de clusters
+
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
+
+plt.title('Dendograma')
+plt.xlabel('Clientes')
+plt.ylabel('Distancias Euclidianas')
+plt.show()
+
+# Ajustando Clustering Jerárquico al conjunto de datos
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage = 'ward')
+
+y_hc = hc.fit_predict(X)
+
+# Visualising the clusters
+plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
+plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
+plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
+plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
